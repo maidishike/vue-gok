1f596aa0bc554a17235168ccc19f3fec7599f2d4
Toby <wsgtracy@163.com>
2017-02-28 17:38:01 +0800

    add vue-resource

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
 import Hello from '@/components/Hello'
 
 Vue.use(Router)
+Vue.use(VueResource)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Hello',
+    component: Hello
+  }]
 })
